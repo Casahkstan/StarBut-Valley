645672836e6984be3b595fbc75989b6ee922016b
votrenom <odde.tony@hotmail.com>
2023-12-04 18:32:15 +0100

    création unité seed managment seed

--- SeedManagmentUnit.pas
+++ SeedManagmentUnit.pas
@@ -5,8 +5,6 @@ interface
 // Init : Initialise les 30 emplacements de notre ferme
 procedure initGarden();
 
-// Plant : Place dans un des emplacement une graine passé en paramètre
-procedure plant(var slot:emplacement; seed:graine);
-
-
 implementation
+
+
