3aa581f00a2777c2c5a7fdc321a578bf5654c1b6
Casahkstan <sacha.laurent71@gmail.com>
2023-12-04 11:27:00 +0100

    passage en variable 'global'

--- playerUnit.pas
+++ playerUnit.pas
@@ -1,141 +1,103 @@
 unit playerUnit;
 
 interface
-// Type pour gérer le joueur
-type
-  Player = record
-	username : String;
-	money : Integer;
-	stamina : Integer;
-	experience : Integer;
-  end;
+// Init: Crée le joueur en lui assignant les valeurs par défault
+procedure initPlayer();
 
-{
-  Init: Crée le joueur en lui assignant les valeurs par défault
-}
-procedure initPlayer(var p : Player);
 
-{
-  Setter : Change le nom de l'utilisateur passé en paramètre
-}
-procedure setName(var p : Player; username : String);
+// Setter : Change le nom de l'utilisateur passé en paramètre
+procedure setName(username : String);
 
-{
-  Setter : Change la quantité d'argent de l'utilisateur passé en paramètre
-}
-procedure setMoney(var p : Player; m : Integer);
+// Setter : Change la quantité d'argent de l'utilisateur passé en paramètre
+procedure setMoney(m : Integer);
 
-{
-  Setter : Change la stamina de l'utilisateur passé en paramètre
-}
-procedure setStamina(var p : Player; s : Integer);
+// Setter : Change la stamina de l'utilisateur passé en paramètre
+procedure setStamina(s : Integer);
 
-{
-  Setter : Change la quantité d'experience de l'utilisateur passé en paramètre
-}
-procedure setExp(var p : Player; e : Integer);
+// Setter : Change la quantité d'experience de l'utilisateur passé en paramètre
+procedure setExp(e : Integer);
 
-{
-  Getter : Retourne le nom de l'utilisateur passé en paramètre
-  ⚠️ Ne peut pas être utiliser pour modifier des variables
-}
-function getName(var p : Player) : String;
+// Getter : Retourne le nom de l'utilisateur passé en paramètre
+// ⚠️ Ne peut pas être utiliser pour modifier des variables
+function getName() : String;
 
-{
-  Getter : Retourne la money de l'utilisateur passé en paramètre
-  ⚠️ Ne peut pas être utiliser pour modifier des variables
-}
-function getMoney(var p : Player) : Integer;
+// Getter : Retourne la money de l'utilisateur passé en paramètre
+// ⚠️ Ne peut pas être utiliser pour modifier des variables
+function getMoney() : Integer;
 
-{
-  Getter : Retourne la stamina de l'utilisateur passé en paramètre
-  ⚠️ Ne peut pas être utiliser pour modifier des variables
-}
-function getStamina(var p : Player) : Integer;
+// Getter : Retourne la stamina de l'utilisateur passé en paramètre
+// ⚠️ Ne peut pas être utiliser pour modifier des variables
+function getStamina() : Integer;
 
-{
-  Getter : Retourne la quantité d'experience de l'utilisateur passé en paramètre
-  ⚠️ Ne peut pas être utiliser pour modifier des variables
-}
-function getExp(var p : Player) : Integer;
+// Getter : Retourne la quantité d'experience de l'utilisateur passé en paramètre
+// ⚠️ Ne peut pas être utiliser pour modifier des variables
+function getExp() : Integer;
 
 implementation
-{
-  Init: Crée le joueur en lui assignant les valeurs par défault
-}
-procedure initPlayer(var p : Player);
+//  Variable du joueur
+var
+  username : String;
+  stamina,
+  money : Integer;
+
+// Init: Crée le joueur en lui assignant les valeurs par défault
+procedure initPlayer();
 begin
-  p.username := 'Non défini';
-  p.stamina := 100;
-  p.money := 200;
+  username := 'Non défini';
+  stamina := 100;
+  money := 200;
 end;
 
-{
-  Setter : Change le nom de l'utilisateur passé en paramètre
-}
-procedure setName(var p : Player; username : String);
+// Setter : Change le nom de l'utilisateur passé en paramètre
+procedure setName(username : String);
 begin
-  p.username := username;
+  username := username;
 end;
 
-{
-  Setter : Change la quantité d'argent de l'utilisateur passé en paramètre
-}
-procedure setMoney(var p : Player; m : Integer);
+// Setter : Change la quantité d'argent de l'utilisateur passé en paramètre
+procedure setMoney(m : Integer);
 begin
-  p.money := m;
+  money := m;
 end;
 
-{
-  Setter : Change la stamina de l'utilisateur passé en paramètre
-}
-procedure setStamina(var p : Player; s : Integer);
+// Setter : Change la stamina de l'utilisateur passé en paramètre
+procedure setStamina(s : Integer);
 begin
-  p.stamina := s;
+  stamina := s;
 end;
 
-{
-  Setter : Change la quantité d'experience de l'utilisateur passé en paramètre
-}
-procedure setExp(var p : Player; e : Integer);
+// Setter : Change la quantité d'experience de l'utilisateur passé en paramètre
+procedure setExp(e : Integer);
 begin
-  p.experience := e;
+  experience := e;
 end;
 
-{
-  Getter : Retourne le nom de l'utilisateur passé en paramètre
-  ⚠️ Ne peut pas être utiliser pour modifier des variables
-}
-function getName(var p : Player) : String;
+// Getter : Retourne le nom de l'utilisateur passé en paramètre
+// ⚠️ Ne peut pas être utiliser pour modifier des variables
+function getName() : String;
 begin
-  getName := p.username;
+  getName := username;
 end;
 
-{
-  Getter : Retourne la money de l'utilisateur passé en paramètre
-  ⚠️ Ne peut pas être utiliser pour modifier des variables
-}
-function getMoney(var p : Player) : Integer;
+// Getter : Retourne la money de l'utilisateur passé en paramètre
+// ⚠️ Ne peut pas être utiliser pour modifier des variables
+function getMoney() : Integer;
 begin
-  getMoney := p.money;
+  getMoney := money;
 end;
 
-{
-  Getter : Retourne la stamina de l'utilisateur passé en paramètre
-  ⚠️ Ne peut pas être utiliser pour modifier des variables
-}
-function getStamina(var p : Player) : Integer;
+// Getter : Retourne la stamina de l'utilisateur passé en paramètre
+// ⚠️ Ne peut pas être utiliser pour modifier des variables
+function getStamina() : Integer;
 begin
-  getStamina := p.stamina;
+  getStamina := stamina;
 end;
 
-{
-  Getter : Retourne la quantité d'experience de l'utilisateur passé en paramètre
-  ⚠️ Ne peut pas être utiliser pour modifier des variables
-}
-function getExp(var p : Player) : Integer;
+// Getter : Retourne la quantité d'experience de l'utilisateur passé en paramètre
+// ⚠️ Ne peut pas être utiliser pour modifier des variables
+function getExp() : Integer;
 begin
-  getExp := p.experience;
+  getExp := experience;
 end;
 
 end.
